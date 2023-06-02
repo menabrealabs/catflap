fa07df4d01808a08f10e92951f2dd83d422791cf
Nyk Cowham <nyk@cowham.net>
2023-06-03 02:17:28 +0700

    Made subtests for port_test.

--- test/port_test.go
+++ test/port_test.go
@@ -6,28 +6,84 @@ import (
 	"github.com/menabrealabs/catflap/internal/pkg/port"
 )
 
-func TestPortAdd_adds_tcp_port_to_set(t *testing.T) {
+var (
+	tcpCase = port.Port{Proto: port.TCP, Number: 22}
+	udpCase = port.Port{Proto: port.UDP, Number: 25}
+)
+
+func setupPorts() port.Set {
 	ports := port.Set{}
 	ports.Add(port.TCP, 22)
 	ports.Add(port.UDP, 25)
 
-	expected := port.Port{Proto: port.TCP, Number: 22}
-	_, found := ports[expected]
-
-	if !found {
-		t.Error("port did not get added to the set")
-	}
+	return ports
 }
 
-func TestPortAdd_adds_udp_port_to_set(t *testing.T) {
-	ports := port.Set{}
-	ports.Add(port.TCP, 22)
-	ports.Add(port.UDP, 25)
+func TestPortAdd(t *testing.T) {
+	ports := setupPorts()
 
-	expected := port.Port{Proto: port.UDP, Number: 25}
-	_, found := ports[expected]
+	t.Run("should add TCP port to set", func(t *testing.T) {
+		_, found := ports[tcpCase]
+		if !found {
+			t.Fail()
+		}
+	})
 
-	if !found {
-		t.Error("port did not get added to the set")
-	}
+	t.Run("should add UDP port to set", func(t *testing.T) {
+		_, found := ports[udpCase]
+		if !found {
+			t.Fail()
+		}
+	})
+}
+
+func TestPortRemove(t *testing.T) {
+	ports := setupPorts()
+	t.Run("should delete TCP port from set", func(t *testing.T) {
+		ports.Remove(tcpCase)
+
+		_, found := ports[tcpCase]
+		if found {
+			t.Error("port did not get removed from the set")
+		}
+	})
+
+	t.Run("should delete UDP port from set", func(t *testing.T) {
+		port := udpCase
+		ports.Remove(port)
+
+		_, found := ports[port]
+		if found {
+			t.Error("port did not get removed from the set")
+		}
+	})
+
+	t.Run("should not delete other ports from set", func(t *testing.T) {
+		ports := setupPorts()
+		ports.Remove(tcpCase)
+
+		_, found := ports[udpCase]
+		if !found {
+			t.Error("port removed from set when other port removed")
+		}
+	})
+}
+
+func TestPortContains(t *testing.T) {
+	ports := setupPorts()
+	t.Run("should return true when port is in the set", func(t *testing.T) {
+		ports.Add(port.TCP, 22)
+
+		if ports.Contains(tcpCase) == false {
+			t.Error("failed to report that a port exists in the set")
+		}
+	})
+
+	t.Run("should return false when port is not in the set", func(t *testing.T) {
+		ports.Remove(tcpCase)
+
+		if ports.Contains(tcpCase) == true {
+			t.Error("failed to report that a port exists in the set")
+		}
+	})
 }
